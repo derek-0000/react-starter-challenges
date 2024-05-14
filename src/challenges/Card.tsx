ea3e4b49498b4e15890b59309e15467ff7191cea
Vanessa Enriquez <137115483+macatops@users.noreply.github.com>
2024-05-14 10:38:24 -0600

    Update card

--- src/challenges/Card.tsx
+++ src/challenges/Card.tsx
@@ -1,35 +1,8 @@
-import { Button, Container,Stack } from "@mui/material";
-// Add required imports here
-import { useState } from "react";
+
 export default function Card() {
-    const [content, setContent] = useState(false)
-
-    const opti
-
-    function Show(){
-        
-        const text = 'Hola';
-
-        return (
-            <div>
-                
-            </div>
-        )
-    }
-
-    function handleClick(){
-        setContent(Show)
-    }
-
+ 
     return (
-    <Container>
-        <h1>Card</h1>
-        <Stack spacing={2}> 
-            <Button variant='contained' onClick={handleClick}>{option}</Button>
-
-        </Stack>
-        
-    </Container>
+        "card"
   );
 }
 
