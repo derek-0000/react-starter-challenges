4ca98298b81542d5eb5b73958841394cf3d35904
Vanessa Enriquez <137115483+macatops@users.noreply.github.com>
2024-05-14 09:49:22 -0600

    Random-color

--- src/challenges/RandomColorGenerator.tsx
+++ src/challenges/RandomColorGenerator.tsx
@@ -1,7 +1,26 @@
-import { Container, Stack, Typography } from "@mui/material";
+import { Button, Container, Stack, Typography, Box } from "@mui/material";
 // Add required imports here
+import { useState } from "react";
 export default function RandomColorGenerator() {
-  // Add your code here
+  
+  const [color, setColor] = useState('#000000');
+
+  function hexa(){
+    let num;
+    const color = [];
+
+    for (let i = 0; i < 6; i++) {
+      num = Math.floor(Math.random()*10);
+      color.push(num);
+    }
+
+    return ('#' +(color.join('')));
+  }
+
+  function handleClick(){
+    setColor(hexa)
+  }
+
   return (
     <Container>
       <Stack>
@@ -9,7 +28,12 @@ export default function RandomColorGenerator() {
           Random Color Generator
         </Typography>
       </Stack>
-      <div>{/* Add your code here */}</div>
+      <div style={{alignItems:'center', justifyContent:'center'}}>{/* Add your code here */}
+        <Box sx={{bgcolor: color, width: 80, height: 70, marginBottom: 5,marginTop: 5}}></Box>
+
+        <Button variant="contained" onClick={handleClick}>Color</Button>
+      
+      </div>
     </Container>
   );
 }
