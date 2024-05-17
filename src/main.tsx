cdb296307e41f68f72dc74ad38fbc1ac59f0d9cd
Vanessa Enriquez <137115483+macatops@users.noreply.github.com>
2024-05-17 09:50:42 -0600

    Create Button

--- src/main.tsx
+++ src/main.tsx
@@ -5,31 +5,32 @@ import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import RandomColorGenerator from "./challenges/RandomColorGenerator.tsx";
 import UsingProps from "./challenges/UsingProps.tsx";
 import CardList from "./challenges/CardList.tsx";
+import Layout from "./challenges/Layout.tsx"
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Outlet />, //Add Layout component here
-    children: [
-      { index: true, element: <App /> },
-      {
-        path: "/random-color-generator",
-        element: <RandomColorGenerator />,
-      },
-      {
-        path: "/using-props",
-        element: <UsingProps />,
-      },
-      {
-        path: "/card",
-        element: <CardList />,
-      },
-    ],
-  },
+ {
+   path: "/",
+   element: <Layout><Outlet/></Layout>, //Add Layout component here
+   children: [
+     { index: true, element: <App /> },
+     {
+       path: "/random-color-generator",
+       element: <RandomColorGenerator />,
+     },
+     {
+       path: "/using-props",
+       element: <UsingProps />,
+     },
+     {
+       path: "/card",
+       element: <CardList />,
+     },
+   ],
+ },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+ <React.StrictMode>
+   <RouterProvider router={router} />
+ </React.StrictMode>
 );
